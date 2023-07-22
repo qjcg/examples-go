585c2cd558a4ce409c6b00a5122a5b6589f2321a
John Gosset <john@gossetx.com>
2023-07-22 13:13:03 -0400

    feat: Add testscript tests

---
+++ internal/testscript/testscript_test.go
@@ -0,0 +1,13 @@
+package testing_testscript
+
+import (
+	"testing"
+
+	"github.com/rogpeppe/go-internal/testscript"
+)
+
+func TestRunTestscripts(t *testing.T) {
+	testscript.Run(t, testscript.Params{
+		Dir: "testdata",
+	})
+}
