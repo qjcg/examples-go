b3914a008b9e94a7481294a8f7952885a5a7cd36
John Gosset <john@gossetx.com>
2023-07-23 05:12:56 -0400

    feat: Use testcontainers for nats integration test

--- internal/nats/nats_test.go
+++ internal/nats/nats_test.go
@@ -3,18 +3,50 @@
 package nats
 
 import (
+	"context"
 	"testing"
 
 	"github.com/nats-io/nats.go"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
 )
 
-func TestPublish(t *testing.T) {
-	nc, err := nats.Connect(nats.DefaultURL)
-	if err != nil {
-		t.Fatal(err)
+func newNATSContainer(ctx context.Context) (testcontainers.Container, error) {
+	req := testcontainers.ContainerRequest{
+		Image:        "nats:2",
+		ExposedPorts: []string{"4222/tcp"},
+		WaitingFor:   wait.ForLog("Server is ready"),
 	}
 
-	if err := nc.Publish("foo", []byte("Hello World")); err != nil {
-		t.Fatal(err)
+	return testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+}
+
+func TestWithNATS(t *testing.T) {
+	ctx := context.Background()
+	natsC, err := newNATSContainer(ctx)
+	if err != nil {
+		t.Fatalf("failed to create NATS container: %v", err)
+	}
+	defer func() {
+		if err := natsC.Terminate(ctx); err != nil {
+			t.Fatalf("failed to terminate container: %s", err.Error())
+		}
+	}()
+
+	endpoint, err := natsC.Endpoint(ctx, "")
+	if err != nil {
+		t.Fatalf("failed to get NATS endpoint: %v", err)
+	}
+
+	nc, err := nats.Connect(endpoint)
+	if err != nil {
+		t.Fatalf("Error connecting to NATS: %v", err)
+	}
+
+	if err := nc.Publish("test", []byte("Hello NATS!")); err != nil {
+		t.Fatalf("failed to publish to NATS: %v", err)
 	}
 }
