08c8babcef8dee280a0605d7d57de6a901e2536f
John Gosset <john@gossetx.com>
2023-07-24 06:06:05 -0400

    feat(internal/flag): Add package

---
+++ internal/flag/main_test.go
@@ -0,0 +1,30 @@
+package flag
+
+import (
+	"flag"
+	"testing"
+)
+
+type Conf struct {
+	Format string
+	Number uint
+}
+
+func TestFlagSet(t *testing.T) {
+	var conf Conf
+	want := "toml"
+
+	fs := flag.NewFlagSet("test", flag.ExitOnError)
+	fs.StringVar(&conf.Format, "f", "yaml", "config file format")
+	fs.UintVar(&conf.Number, "n", 42, "number of items")
+	fs.Parse([]string{
+		"-f", want,
+	})
+
+	got := conf.Format
+	if got != want {
+		t.Fatalf("want %v got %v", want, got)
+	}
+
+	t.Logf("%v", conf)
+}
