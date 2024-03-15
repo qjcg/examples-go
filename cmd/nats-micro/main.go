6c3524244d056cebc2e1aa2fc169d113819cd708
John Gosset <john@gossetx.com>
2024-03-14 21:15:32 -0400

    feat: Add `nats-micro` example

---
+++ cmd/nats-micro/main.go
@@ -0,0 +1,31 @@
+// A minimal NATS micro service.
+package main
+
+import (
+	"flag"
+
+	"github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go/micro"
+)
+
+func main() {
+	flagURL := flag.String("u", nats.DefaultURL, "nats URL")
+	flag.Parse()
+
+	nc, _ := nats.Connect(*flagURL)
+
+	// request handler
+	echoHandler := func(req micro.Request) {
+		req.Respond(req.Data())
+	}
+
+	_, err := micro.AddService(nc, micro.Config{
+		Name:    "EchoService",
+		Version: "1.0.0",
+		// base handler
+		Endpoint: &micro.EndpointConfig{
+			Subject: "svc.echo",
+			Handler: micro.HandlerFunc(echoHandler),
+		},
+	})
+}
