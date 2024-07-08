e2fcfd1a1256b0da24b2b045c179d18a684d1550
John Gosset <john@gossetx.com>
2024-07-08 13:56:59 -0400

    feat(playwright): Convert to f-tests

--- internal/playwright/main_test.go
+++ internal/playwright/main_test.go
@@ -3,39 +3,81 @@
 package playwright
 
 import (
+	"fmt"
+	"net/url"
 	"testing"
 
+	"github.com/gosimple/slug"
 	"github.com/playwright-community/playwright-go"
+)
+
+type Browser int
+
+const (
+	Chromium Browser = iota
+	Firefox
+	WebKit
 )
 
 // https://github.com/playwright-community/playwright-go/blob/main/examples/screenshot/main.go
 func TestScreenshot(t *testing.T) {
-	pw, err := playwright.Run()
-	if err != nil {
-		t.Fatalf("could not launch playwright: %v", err)
+	f := func(ssURL string, browserType Browser) {
+		t.Helper()
+
+		pw, err := playwright.Run()
+		if err != nil {
+			t.Fatalf("could not launch playwright: %v", err)
+		}
+
+		var browser playwright.Browser
+		switch browserType {
+		case Chromium:
+			browser, err = pw.Chromium.Launch()
+		case Firefox:
+			browser, err = pw.Firefox.Launch()
+		case WebKit:
+			browser, err = pw.WebKit.Launch()
+		}
+
+		if err != nil {
+			t.Fatalf("could not launch browser: %v", err)
+		}
+
+		page, err := browser.NewPage()
+		if err != nil {
+			t.Fatal(err)
+		}
+		if _, err = page.Goto(ssURL, playwright.PageGotoOptions{
+			WaitUntil: playwright.WaitUntilStateDomcontentloaded,
+		}); err != nil {
+			t.Fatalf("could not goto: %v", err)
+		}
+
+		parsedURL, err := url.Parse(ssURL)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		urlSlug := slug.Make(parsedURL.Host + parsedURL.Path)
+		ssFileName := fmt.Sprintf("%s.png", urlSlug)
+
+		if _, err = page.Screenshot(playwright.PageScreenshotOptions{
+			Path: playwright.String(ssFileName),
+		}); err != nil {
+			t.Fatalf("could not create screenshot: %v", err)
+		}
+
+		t.Logf("SCREENSHOT %s", ssFileName)
+
+		if err = browser.Close(); err != nil {
+			t.Fatalf("could not close browser: %v", err)
+		}
+		if err = pw.Stop(); err != nil {
+			t.Fatalf("could not stop Playwright: %v", err)
+		}
 	}
-	browser, err := pw.Chromium.Launch()
-	if err != nil {
-		t.Fatalf("could not launch Chromium: %v", err)
-	}
-	page, err := browser.NewPage()
-	if err != nil {
-		t.Fatal(err)
-	}
-	if _, err = page.Goto("https://news.ycombinator.com/", playwright.PageGotoOptions{
-		WaitUntil: playwright.WaitUntilStateDomcontentloaded,
-	}); err != nil {
-		t.Fatalf("could not goto: %v", err)
-	}
-	if _, err = page.Screenshot(playwright.PageScreenshotOptions{
-		//Path: playwright.String("screenshot.png"),
-	}); err != nil {
-		t.Fatalf("could not create screenshot: %v", err)
-	}
-	if err = browser.Close(); err != nil {
-		t.Fatalf("could not close browser: %v", err)
-	}
-	if err = pw.Stop(); err != nil {
-		t.Fatalf("could not stop Playwright: %v", err)
-	}
+
+	f("https://news.ycombinator.com/", Chromium)
+	f("https://news.google.com/", Firefox)
+	f("https://github.com/playwright-community/playwright-go", WebKit)
 }
