c5fdfb58f0e60ac71e9ab006cb45f704ba6a430a
John Gosset <john@gossetx.com>
2025-07-04 16:05:42 -0400

    refactor: Move get and post handlers to separate methods

--- cmd/http-webapp-sqlite/main.go
+++ cmd/http-webapp-sqlite/main.go
@@ -26,6 +26,10 @@ const schema = `
 	);
 `
 
+type App struct {
+	db *sql.DB
+}
+
 func main() {
 	dbFile := flag.String("f", "example.db?_journal_mode=wal", "sqlite DB file")
 	flag.Parse()
@@ -40,42 +44,49 @@ func main() {
 		}
 	}()
 
+	app := App{db: db}
+
 	ctx := context.Background()
-	if _, err := db.ExecContext(ctx, schema); err != nil {
+	if _, err := app.db.ExecContext(ctx, schema); err != nil {
 		log.Fatal(err)
 	}
 
-	if _, err := db.ExecContext(ctx, `INSERT OR IGNORE INTO visits VALUES (1, 0);`); err != nil {
+	if _, err := app.db.ExecContext(ctx, `INSERT OR IGNORE INTO visits VALUES (1, 0);`); err != nil {
 		log.Fatal(err)
 	}
 
-	var visits Visits
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		err := db.QueryRowContext(ctx, `SELECT count FROM visits;`).Scan(&visits.Count)
-		if err != nil {
-			msg := fmt.Sprintf("unable to SELECT from visits table: %v", err)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-
-		w.Header().Set("Content-type", "application/json")
-		err = json.NewEncoder(w).Encode(&visits)
-		if err != nil {
-			msg := fmt.Sprintf("error encoding visits: %v", err)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-	})
-
-	http.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
-		_, err := db.ExecContext(ctx, `UPDATE visits SET count = count + 1 WHERE id = 1;`)
-		if err != nil {
-			msg := fmt.Sprintf("unable to UPDATE count in sqlite visits table: %v", err)
-			http.Error(w, msg, http.StatusInternalServerError)
-			return
-		}
-	})
+	http.HandleFunc("GET /", app.handleGetVisits)
+	http.HandleFunc("POST /", app.handleUpdateVisits)
 
 	fmt.Println("Listening on http://0.0.0.0:9999")
 	log.Fatal(http.ListenAndServe(":9999", nil))
+}
+
+func (a *App) handleGetVisits(w http.ResponseWriter, r *http.Request) {
+	var visits Visits
+	ctx := context.Background()
+	err := a.db.QueryRowContext(ctx, `SELECT count FROM visits;`).Scan(&visits.Count)
+	if err != nil {
+		msg := fmt.Sprintf("unable to SELECT from visits table: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	err = json.NewEncoder(w).Encode(&visits)
+	if err != nil {
+		msg := fmt.Sprintf("error encoding visits: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+}
+
+func (a *App) handleUpdateVisits(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
+	_, err := a.db.ExecContext(ctx, `UPDATE visits SET count = count + 1 WHERE id = 1;`)
+	if err != nil {
+		msg := fmt.Sprintf("unable to UPDATE count in sqlite visits table: %v", err)
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
 }
