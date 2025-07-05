69dd170b47fd5f75f67eb1fd606f92c877e7d080
John Gosset <john@gossetx.com>
2025-07-05 10:38:33 -0400

    feat: Add pragmas

--- cmd/db-sqlc/schema.sql
+++ cmd/db-sqlc/schema.sql
@@ -1,3 +1,7 @@
+PRAGMA foreign_keys = 1;
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = NORMAL;
+
 CREATE TABLE IF NOT EXISTS authors (
   id   INTEGER PRIMARY KEY,
   name TEXT    UNIQUE NOT NULL,
