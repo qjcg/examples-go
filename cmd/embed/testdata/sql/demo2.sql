43f3e4e1ecc3e08fc73f014e6cdc780409b7b408
John Gosset <john@gossetx.com>
2025-07-07 04:49:04 -0400

    feat: Add embed tests and move to stdlib

--- cmd/embed/testdata/sql/demo2.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE radios (
-	id int primary key
-	name text
-);
---
+++ stdlib/embed/embed_test.go
@@ -0,0 +1,62 @@
+package embed
+
+import (
+	"bytes"
+	"embed"
+	"io/fs"
+	"testing"
+)
+
+//go:embed testdata/greeting.txt
+var greeting string
+
+//go:embed testdata/greeting.txt
+var greetingBytes []byte
+
+//go:embed testdata/greetings
+var greetingsFS embed.FS
+
+func Test_embedString(t *testing.T) {
+	got := greeting
+	want := "hello\n"
+	if got != want {
+		t.Fatalf("want: %s, got: %s", want, got)
+	}
+}
+
+func Test_embedByteSlice(t *testing.T) {
+	got := greetingBytes
+	want := []byte("hello\n")
+	if !bytes.Equal(got, want) {
+		t.Fatalf("want: %s, got: %s", want, got)
+	}
+}
+
+func Test_embedFS(t *testing.T) {
+	t.Run("ReadFile", func(t *testing.T) {
+		got, err := greetingsFS.ReadFile("testdata/greetings/fr.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := []byte("bonjour\n")
+		if !bytes.Equal(got, want) {
+			t.Fatalf("want: %s, got: %s", want, got)
+		}
+	})
+
+	t.Run("SubFS", func(t *testing.T) {
+		subFS, err := fs.Sub(greetingsFS, "testdata/greetings")
+
+		got, err := fs.ReadFile(subFS, "fr.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		want := []byte("bonjour\n")
+		if !bytes.Equal(got, want) {
+			t.Fatalf("want: %s, got: %s", want, got)
+		}
+	})
+
+}
